e27dc180036f16a46ea8bf55fa3c8715b7a6daf5
JamieRL <lee.jamr@gmail.com>
2018-06-06 15:05:03 -0700

    testing version checker with travis

---
+++ check_version.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+from ubyssey import settings
+
+
+with open('ubyssey/static/package.json') as f:
+    data = json.load(f)
+    if settings.VERSION != data['version']:
+        print('wrong version')
+        sys.exit(1)
+    else:
+        print('version ok')
