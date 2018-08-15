a0ec905b25fb4fe12361942aba7af0adef65da42
rowansdabomb <rowanskye@gmail.com>
2018-08-14 17:41:40 -0700

    add simple cart

--- ubyssey/static/src/js/components/Store/Store.jsx
+++ ubyssey/static/src/js/components/Store/Store.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import DispatchAPI from '../../api/dispatch'
 
-
 class Store extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       selected: null,
       products: [],
-      catalogueView: true
+      catalogueView: true,
+      showCart: false
     }
   }
   componentDidMount() {
@@ -18,6 +18,11 @@ class Store extends React.Component {
         products: response.results
       })
     })
+
+    // simpleCart.bind( 'ready' , function(){
+    //   console.log( "simpleCart total: " + simpleCart.toCurrency( simpleCart.total() ) ); 
+    // });
+    console.log(simpleCart)
   }
 
   getAbsoluteURL(imageURL) {
@@ -25,46 +30,82 @@ class Store extends React.Component {
     return URL.concat(imageURL)
   }
 
-  renderProduct(data) {
-    console.log(data)
+  toggleView(index) {
+    this.setState(prevState => ({
+      catalogueView: !prevState.catalogueView,
+      selected: index
+    }))
+  }
+
+  addToCart(data) {
+    simpleCart.add({ 
+      name: data.name,
+      price: data.price,
+      size: data.size || null,
+      quantity: data.quantity
+    });
+  }
+
+  renderProduct(product, index) {
     const productStyle = {
-      backgroundImage: 'url(' + this.getAbsoluteURL(data.image_url) + ')',
+      backgroundImage: 'url(' + this.getAbsoluteURL(product.image_url) + ')',
     }
     return (
-      <div className='c-product__catalogue-container'>
-        <div 
-          className='c-product__catalogue-content' 
-          style={productStyle}>
-          {data.description}
-          {data.price}
-          {data.quantity}
+      <div 
+        key={index}
+        onClick={() => this.toggleView(index)}
+        className='c-product__catalogue-wrapper'>
+        <div className='c-product__catalogue-container'>
+          <div 
+            className='c-product__catalogue-content' 
+            style={productStyle}>
+            <span>{product.title}</span>
+            <span>{product.price}</span>
+          </div>
         </div>
       </div>
     )
   }
 
-  renderProductView() {
+  renderProductView(data) {
     const product = this.state.products[this.state.selected]
     return (
       <div className='c-store-product-wrapper__full'>
-        <div className='c-product-image' style={{backgroundImage: 'url(' + this.getAbsoluteURL(product.image_url)}} />
+        <div 
+          onClick={()=> this.toggleView(null)}
+          className='c-button c-button--small'>
+          Go Back
+        </ div>
+        <div className="simpleCart_shelfItem">
+          <h2 className="item_name"> Awesome T-shirt </h2>
+          <input type="text" value="1" className="item_Quantity" />
+          <span className="item_price">$35.99</span>
+          <a className="item_add" href="javascript:;"> Add to Cart </a>
+        </div>
+        {/* <div className='c-product-image' style={{backgroundImage: 'url(' + this.getAbsoluteURL(product.image_url)}} />
         <div className='c-product__description'>
-          {data.description}
+          {product.description}
         </div>
         <div className='c-product__price'>
-          {data.price}
+          {product.price}
         </div>
         <div className='c-product__quantity'>
-          {data.quantity}
+          Stock: {product.quantity}
         </div>
+        <div className='c-button c-button--small'
+          onClick={()=>this.addToCart(product)}>Add to Cart</div> */}
+        <div 
+          onClick={()=>{this.setState({showCart: true})}}
+          className='c-button c-button--small'>View Cart</div>
+        {this.state.showCart && <div className="simpleCart_items"></div>}
       </div>
     )
   }
 
   renderCatalogueView() {
     return (
-      this.state.products.map((data) => {
-        return this.renderProduct(data)
+      this.state.products.map((data, index) => {
+        return this.renderProduct(data, index)
       })
     )
   }
