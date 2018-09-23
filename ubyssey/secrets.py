ad6c39bed2ddbf7942b06424b8840683db23d839
rowansdabomb <rowanskye@gmail.com>
2018-09-23 16:10:31 -0700

    create secrets class, include travis gcs upload

--- ubyssey/secrets.py
+++ ubyssey/secrets.py
@@ -1,12 +1,13 @@
 from google.cloud import datastore
 
-# Instantiates a client
-client = datastore.Client('ubyssey-prd-flex')
+class Secrets():
+    client = datastore.Client('ubyssey-prd-flex')
 
-def get(client, key):
-    query = client.query(kind='secrets')
-    secrets = list(query.fetch())
-    for secret in secrets:
-        if secret['key'] == key:
-            return secret['value']
-    return 'Failed to find value in secrets'
+    @staticmethod
+    def get(client, key):
+        query = client.query(kind='secrets')
+        secrets = list(query.fetch())
+        for secret in secrets:
+            if secret['key'] == key:
+                return secret['value']
+        return query
